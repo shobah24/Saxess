ca72fec601bd30af437d64a8639b4a340ba6b125
HugoW5 <hugo05.wakt@gmail.com>
2025-01-28 13:24:29 +0100

    Add AddNewTreatmentMenu class with ViewMenu method
    Introduce AddNewTreatmentMenu class in Saxess namespace, inheriting from Menu and overriding ViewMenu method. The method interacts with the user to add a new treatment, including validation and exception handling. Added necessary using directives.

---
+++ Saxess/AddNewTreatmentMenu.cs
@@ -0,0 +1,67 @@
+﻿using Saxess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saxess
+{
+    internal class AddNewTreatmentMenu : Menu
+    {
+        public override void ViewMenu()
+        {
+            using (var context = new SassexDbContext())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Enter the name of the new treatment:");
+                        var treatmentName = Console.ReadLine();
+                        foreach (var _treatment in context.Treatments)
+                        {
+                            if (_treatment.TreatmentName == treatmentName)
+                            {
+                                throw new Exception("Treatment already exists");
+                            }
+                        }
+                        if (treatmentName.Trim() == "")
+                        {
+                            throw new Exception("Treatment name cannot be empty");
+                        }
+                        Console.Write("Enter the treatment type:");
+                        var treatmentType = Console.ReadLine();
+                        if (treatmentType.Trim() == "")
+                        {
+                            throw new Exception("Treatment type cannot be empty");
+                        }
+
+                        var treatment = new Treatment
+                        {
+                            TreatmentName = treatmentName,
+                            TreatmentType = treatmentType
+                        };
+
+                        context.Treatments.Add(treatment);
+                        context.SaveChanges();
+                        Console.WriteLine("Added new treatment");
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(e.Message);
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine("Press any key to continue");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+            }
+        }
+    }
+}
