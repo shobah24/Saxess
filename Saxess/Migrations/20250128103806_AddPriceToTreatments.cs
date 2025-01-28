4b15cc428bec529acf102816edf4b4cc1ded2022
Edgarskonnegard <j.edgarskonnegard@gmail.com>
2025-01-28 11:48:00 +0100

    Added Price
    lade till Price

---
+++ Saxess/Migrations/20250128103806_AddPriceToTreatments.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Saxess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToTreatments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Customer__A4AE64B8EC55B8F1", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    LastName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Employee__7AD04FF1B387E98D", x => x.EmployeeID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Treatments",
+                columns: table => new
+                {
+                    TreatmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TreatmentName = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    TreatmentType = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Treatmen__1A57B71188B55E49", x => x.TreatmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    AppointmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AppointmentDatetime = table.Column<DateTime>(type: "datetime", nullable: true),
+                    EmployeeID = table.Column<int>(type: "int", nullable: true),
+                    CustomerID = table.Column<int>(type: "int", nullable: true),
+                    TreatmentID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Appointm__8ECDFCA206FA32F4", x => x.AppointmentID);
+                    table.ForeignKey(
+                        name: "FK__Appointme__Custo__5070F446",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID");
+                    table.ForeignKey(
+                        name: "FK__Appointme__Emplo__4F7CD00D",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeID");
+                    table.ForeignKey(
+                        name: "FK__Appointme__Treat__5165187F",
+                        column: x => x.TreatmentID,
+                        principalTable: "Treatments",
+                        principalColumn: "TreatmentID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeesTreatments",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false),
+                    TreatmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__Employee__7B753480ED96E5E5", x => new { x.EmployeeID, x.TreatmentID });
+                    table.ForeignKey(
+                        name: "FK__Employees__Treat__5441852A",
+                        column: x => x.EmployeeID,
+                        principalTable: "Employees",
+                        principalColumn: "EmployeeID");
+                    table.ForeignKey(
+                        name: "FK__Employees__Treat__5535A963",
+                        column: x => x.TreatmentID,
+                        principalTable: "Treatments",
+                        principalColumn: "TreatmentID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_CustomerID",
+                table: "Appointments",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_EmployeeID",
+                table: "Appointments",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_TreatmentID",
+                table: "Appointments",
+                column: "TreatmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeesTreatments_TreatmentID",
+                table: "EmployeesTreatments",
+                column: "TreatmentID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "EmployeesTreatments");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Treatments");
+        }
+    }
+}
